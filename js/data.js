e3e21aed7979a7b20e0785a3ff3ec5e3c039412d
kuzuri1709 <74127346+kuzuri1709@users.noreply.github.com>
2020-11-22 15:55:25 +0700

    ff

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const DATA = {
+    level_1: [
+        {
+            question: '1 + 1 = ?',
+            options: ['1', '2', '3', '4'],
+            correctAnswer: 1
+        },
+        {
+    
+        }
+    ],
+
+    level_2: [
+
+    ]
+    
+}
+
+
+let user = [
+    {
+        fullName: 'Nguyen Van A',
+        username: 'Van A',
+        password: 'Vana1223@' // At least 1 uppercase, 1 number, 1 lowercase, 1 special character
+    }
+]
+
+// DATA[0].options[DATA.correctAnswer]
