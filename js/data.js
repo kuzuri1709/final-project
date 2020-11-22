a3a6867646db54bc0f4c2d8c0bafcf1bddcfbb9c
kuzuri1709 <74127346+kuzuri1709@users.noreply.github.com>
2020-11-22 17:10:48 +0700

    f

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const DATA = {
         {
             question: '1 + 1 = ?',
             options: ['1', '2', '3', '4'],
-            correctAnswer: 1
+            correctAnswer: options[1]
         },
         {
     
@@ -17,12 +17,39 @@ const DATA = {
 }
 
 
-let user = [
-    {
-        fullName: 'Nguyen Van A',
-        username: 'Van A',
-        password: 'Vana1223@' // At least 1 uppercase, 1 number, 1 lowercase, 1 special character
-    }
+let accountInfo = [
+    // {
+    //     fullName: ,
+    //     username: ,
+    //     password:  ,// At least 1 uppercase, 1 number, 1 lowercase, 1 special character
+    //     confirm:
+    // }
 ]
+
+let nameInput = document.getElementById('nameInput')
+let userInput = document.getElementById('userInput')
+let pwInput = document.getElementById('pwInput')
+let cfInput = document.getElementById('cfInput')
+
+let btnAdd = document.getElementById('create')
+
+btnAdd.addEventListener('click', function(){
+    let name = nameInput.value
+    let user = userInput.value
+    let pw = pwInput.value
+    let cf = cfInput.value
+
+    insertData(name, user, pw, cf)
+})
+
+function insertData(name, user, pw, cf){
+    `{
+        fullName: ${name},
+        username: ${user},
+        password:  ${pw},
+        confirm: ${cf}
+    }`
+}
+
 
 // DATA[0].options[DATA.correctAnswer]
