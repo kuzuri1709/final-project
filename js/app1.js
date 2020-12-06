a15eb1596a1dc26a629706347fad5ed6f67d09c5
kuzuri1709 <ctcxre@gmail.com>
2020-12-06 15:02:03 +0700

    f

--- js/app1.js
+++ js/app1.js
@@ -1,6 +1,8 @@
 let options = document.querySelectorAll('.options');
 let quizHeader = document.querySelectorAll('.quizHeader span');
+let questions;
 // let label = document.querySelectorAll('.option .choices')
+
 
 function renderQuestions(attempt) {
     for (let i = 0; i < attempt.length; i++) {
@@ -13,12 +15,52 @@ function renderQuestions(attempt) {
 
             labelOption[j].addEventListener('click', function () {
                 labelOption[j].children[0].checked = true
+
             })
         }
 
 
     }
+
+    questions = attempt;
+    console.log(questions);
+}
+
+function checkAnswer(answers) {
+    let score = 0;
+    for(let i = 0; i < questions.length; i++){
+        if(questions[i].correctAnswer == answers[i]){
+            score ++;
+        }
+    }
+    return score;
 }
 
 
 renderQuestions(randomQuestion(DATA.level_1))
+
+let btnSubmit = document.getElementById('btnSubmit');
+btnSubmit.addEventListener('click', function () {
+
+    let answers = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+    for (let i = 0; i < options.length; i++) {
+        let labelOption = options[i].children;
+
+        for (let j = 0; j < labelOption.length; j++) {
+            if(labelOption[j].children[0].checked == true){
+                answers[i] = j;
+            }
+        }
+    }
+    // console.log(answers);
+
+    let time = document.getElementById('timer').innerHTML;
+    let timeDisplay = time.split(/[:]/)
+    
+    
+    // console.log(timeDisplay)
+})
+
+function stopTimer(){
+    
+}
