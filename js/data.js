f80f706bf48637f398b6d79a7add1dbc03ad87db
vuhamthieu <74126229+vuhamthieu@users.noreply.github.com>
2020-12-13 11:14:25 +0700

    1233

--- js/data.js
+++ js/data.js
@@ -2,8 +2,8 @@ let accountInfo = [
     {
         fullName: 'thieu',
         username: 'handsomeguy',
-        password:  'absolutelybrilliant',// At least 1 uppercase, 1 number, 1 lowercase, 1 special character
-        confirm: 'absolutelybrilliant',
+        password:  'Thieu1807',// At least 1 uppercase, 1 number, 1 lowercase, 1 special character
+        confirm: 'Thieu1807',
     }
 ];
 
